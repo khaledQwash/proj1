44801ab8d8beb82d6e7b5f63788738184a49a0f0
Khaled A Qwash <khaled2002q@gmail.com>
2025-03-09 12:53:03 -0700

    secondProject

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
 Route::get('/', function () {
     return view('welcome');
@@ -24,4 +25,12 @@ Route::post('/about', function () {
         '3'=> 'development3',
     ];
     return view('about', compact('name', 'departments'));
+});
+Route::get('tasks', function () {
+    return view('tasks');
+});
+Route::post('create', function () {
+    $task_name = $_POST['name'];
+    DB::table('tasks')->insert(['name'=> $task_name]);
+    return view('tasks');
 });
